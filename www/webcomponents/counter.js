8908fd8b16cb641b55e9866348b25de6b2f3ab68
System Administrator <root@ES2BR-M0WN0HHHV.tandem.ddb>
2022-12-21 18:12:40 +0100

    working on counter

--- www/webcomponents/counter.js
+++ www/webcomponents/counter.js
@@ -3,16 +3,58 @@ export default class CounterComponent extends HTMLElement {
         super()
         this.increase = this.increase.bind(this)
         this.decrease = this.decrease.bind(this)
+        this.increaseStep = this.increaseStep.bind(this)
+        this.decreaseStep = this.decreaseStep.bind(this)
+        this.increaseMax = this.increaseMax.bind(this)
+        this.decreaseMax = this.decreaseMax.bind(this)
+        this.increaseMin = this.increaseMin.bind(this)
+        this.decreaseMin = this.decreaseMin.bind(this)
+        this.reset = this.reset.bind(this)
+        this.customizeCount = this.customizeCount.bind(this)
         this.shadow = this.attachShadow({mode: 'open'});
-        this.p = document.createElement('p');
-        this.p.innerText = this.counter
+        this.spanCounter = document.createElement('p');
+        this.spanStep = document.createElement('p');
+        this.spanMax = document.createElement('p');
+        this.spanMin = document.createElement('p');
+        this.spanCounter.innerText = `La cuenta está en: ${this.counter}.`
+        this.spanStep.innerText = `El intervalo es:  ${this.step}`
+        this.spanMax.innerText = `El valor máximo es:  ${this.max}`
+        this.spanMin.innerText = `El valor mínimo es: ${this.min}`;
+        this.summary = document.createElement('div');
+        this.summary.appendChild(this.spanCounter)
+        this.summary.appendChild(this.spanMax)
+        this.summary.appendChild(this.spanMin)
+        this.summary.appendChild(this.spanStep)
         
+        
+        this.customCountButton = document.createElement('button')
+        this.customCountButton.innerText = 'Personalizar Cuenta'
+        this.resetButton = document.createElement('button')
+        this.resetButton.innerText = 'Resetear Cuenta'
         this.increaseButton = document.createElement('button');
         this.decreaseButton = document.createElement('button');
         this.increaseButton.className = 'green'
         this.decreaseButton.className = 'red'
         this.increaseButton.innerText = "+"
         this.decreaseButton.innerText = "-"
+        this.increaseStepButton = document.createElement('button');
+        this.decreaseStepButton = document.createElement('button');
+        this.increaseStepButton.className = 'green'
+        this.decreaseStepButton.className = 'red'
+        this.increaseStepButton.innerText = '+'
+        this.decreaseStepButton.innerText = '-'
+        this.increaseMaxButton = document.createElement('button');
+        this.decreaseMaxButton = document.createElement('button');
+        this.increaseMaxButton.className = 'green'
+        this.decreaseMaxButton.className = 'red'
+        this.increaseMaxButton.innerText = '+'
+        this.decreaseMaxButton.innerText = '-'
+        this.increaseMinButton = document.createElement('button');
+        this.decreaseMinButton = document.createElement('button');
+        this.increaseMinButton.className = 'green'
+        this.decreaseMinButton.className = 'red'
+        this.increaseMinButton.innerText = '+'
+        this.decreaseMinButton.innerText = '-'
 
         let style = `
         <style>
@@ -48,21 +90,38 @@ export default class CounterComponent extends HTMLElement {
             margin: 1rem auto;
             border-radius: 5% / 20%;
             background-color: black
-
+        }
+        .hyde {
+            display: none
+        }
+        .show {
+            display: true
         }
         </style>
         `
 
-
         this.shadow.innerHTML = style
         let divButton = document.createElement('div');
+        this.counters = document.createElement('div');
+        this.counters.className = 'divButton'
         divButton.className = 'divButton'
+        this.counters.appendChild(this.summary)
+        this.counters.appendChild(this.increaseStepButton)
+        this.counters.appendChild(this.decreaseStepButton)
+        this.counters.appendChild(this.increaseMaxButton)
+        this.counters.appendChild(this.decreaseMaxButton)
+        this.counters.appendChild(this.increaseMinButton)
+        this.counters.appendChild(this.decreaseMinButton)
+        this.counters.className = 'hyde'
         divButton.appendChild(this.increaseButton)
         divButton.appendChild(this.decreaseButton)
+        this.shadow.appendChild(this.customCountButton)
+        this.shadow.appendChild(this.resetButton)
         this.shadow.appendChild(divButton)
-        this.shadow.appendChild(this.p)
+        this.shadow.appendChild(this.counters)
     }
     increase(){
+        console.log("holi");
         let step = +this.step;
         let max = +this.max;
         let counter = +this.counter;
@@ -74,18 +133,57 @@ export default class CounterComponent extends HTMLElement {
         }
     }
 
-        decrease(){
-            let step = +this.step;
-            let min = +this.min;
-            let counter = +this.counter;
-            if(counter - step < min) {
-                return
-            } else{
-                counter -=step
-                this.setAttribute('counter', counter)
-            }
+    decrease(){
+        let step = +this.step;
+        let min = +this.min;
+        let counter = +this.counter;
+        if(counter - step < min) {
+            return
+        } else{
+            counter -=step
+            this.setAttribute('counter', counter)
         }
+    }
 
+    customizeCount(){
+        if(this.counters.className === 'hyde') {
+            this.counters.className = 'show'
+        } else {
+            console.log("hola");
+            this.counters.className = 'hyde'
+        }
+    }
+
+    increaseStep(){
+            let step = parseInt(this.step);
+            this.setAttribute('step', step+1)
+    }
+    decreaseStep(){
+            let step = parseInt(this.step);
+            this.setAttribute('step', step-1)
+    }
+    increaseMax(){
+            let max = parseInt(this.max);
+            this.setAttribute('max', max+1)
+    }
+    decreaseMax(){
+            let max = parseInt(this.max);
+            this.setAttribute('max', max-1)
+    }
+    increaseMin(){
+            let min = parseInt(this.min);
+            this.setAttribute('min', min+1)
+    }
+    decreaseMin(){
+            let min = parseInt(this.min);
+            this.setAttribute('min', min-1)
+    }
+    reset(){
+        this.setAttribute('step', 1)
+        this.setAttribute('counter', 0)
+        this.setAttribute('max', 10)
+        this.setAttribute('min', 0)
+    }
 
     //Getters & Setters
     get counter(){
@@ -121,13 +219,32 @@ export default class CounterComponent extends HTMLElement {
     connectedCallback(){
         this.increaseButton.addEventListener('click', this.increase)
         this.decreaseButton.addEventListener('click', this.decrease)
+        this.increaseStepButton.addEventListener('click', this.increaseStep)
+        this.decreaseStepButton.addEventListener('click', this.decreaseStep)
+        this.increaseMaxButton.addEventListener('click', this.increaseMax)
+        this.decreaseMaxButton.addEventListener('click', this.decreaseMax)
+        this.increaseMinButton.addEventListener('click', this.increaseMin)
+        this.decreaseMinButton.addEventListener('click', this.decreaseMin)
+        this.resetButton.addEventListener('click', this.reset)
+        this.customCountButton.addEventListener('click', this.customizeCount)
     }
     attributeChangedCallback(att, oldV, newV){
         const span = document.createElement('span');
         switch(att){
             case "counter":
-                this.p.innerText = this.getAttribute('counter')
-        }
-    }   
+                this.spanCounter.innerText = `La cuenta está en: ${this.getAttribute('counter')}`
+                break;
+            case "step":
+                this.spanStep.innerText = `El intervalo es: ${this.getAttribute('step')}`
+                break;
+            case "max":
+                this.spanMax.innerText = `El valor máximo es:  ${this.getAttribute('max')}`
+                break;
+            case "min":
+                this.spanMin.innerText = `El valor mínimo es: ${this.getAttribute('min')}`;
+                break;
+            }
+    
+    }
 }
 
